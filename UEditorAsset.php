f205e35a8a05ae6ebe13fd0e782950d15276b89b
moxuandi <1104984259@qq.com>
2018-10-04 11:24:28 +0800

    旧版归档

---
+++ UEditorAsset.php
@@ -0,0 +1,28 @@
+<?php
+namespace moxuandi\ueditor;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset bundle for the UEditor
+ *
+ * @author  zhangmoxuan <1104984259@qq.com>
+ * @link  http://www.zhangmoxuan.com
+ * @QQ  1104984259
+ * @Date  2017/7/14
+ */
+class UEditorAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/moxuandi/yii2-ueditor/assets';
+
+    public $css = [];
+
+    public $js = [
+        'ueditor.config.js',
+        'ueditor.all.min.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
